230bd57a3300f30027fa48d79010b6c1e4434975
Shirahamah <hiropi1224s@gmail.com>
2023-04-22 23:26:02 +0900

    💄 css 追加

--- remix-jokes/app/root.tsx
+++ remix-jokes/app/root.tsx
@@ -1,15 +1,34 @@
-import { LiveReload, Outlet } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import { Links, LiveReload, Outlet } from "@remix-run/react";
+
+import globalLargeStylesUrl from "~/styles/global-large.css";
+import globalMediumStylesUrl from "~/styles/global-medium.css";
+import globalStylesUrl from "~/styles/global.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: globalStylesUrl },
+  {
+    rel: "stylesheet",
+    href: globalMediumStylesUrl,
+    media: "print, (min-width: 640px)",
+  },
+  {
+    rel: "stylesheet",
+    href: globalLargeStylesUrl,
+    media: "screen and (min-width: 1024px)",
+  },
+];
 
 export default function App() {
   return (
-    <html lang="ja">
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <title>Remix: So great, it's funny!</title>
+        <Links />
       </head>
       <body>
-        {/* LiveReloadコンポーネントは変更を加えるたびにブラウザーを自動更新するための開発中に役立つ */}
         <Outlet />
         <LiveReload />
       </body>
